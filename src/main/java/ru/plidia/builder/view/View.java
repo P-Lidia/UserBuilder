e81735068c4a4e71eb2b3eec8103b1753548e319
P-Lidia <Lydiasmolyakova@gmail.com>
2025-01-12 16:57:03 +0300

    fix: 12.01.2025 16:56 fix View class

--- src/main/java/ru/plidia/builder/view/View.java
+++ src/main/java/ru/plidia/builder/view/View.java
@@ -7,14 +7,17 @@ import java.util.stream.Stream;
 
 public class View {
     public void printSortList(Stream<User> userStream) {
+        System.out.println("\nAll users: ");
         userStream.forEach(System.out::println);
     }
 
     public void printEvenIdList(List<User> userList) {
+        System.out.println("\nEven-id users: ");
         userList.forEach(System.out::println);
     }
 
     public void printSameIdAndAgeList(Stream<User> userStream) {
+        System.out.println("\nUsers with the same id and age: ");
         userStream.forEach(System.out::println);
     }
 }
