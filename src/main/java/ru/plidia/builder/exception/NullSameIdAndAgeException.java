2772dba09418da616ce3a94d8bb19f96132cf504
P-Lidia <Lydiasmolyakova@gmail.com>
2025-01-12 16:25:33 +0300

    feat: 12.01.2025 16:25 created NullSameIdAndAgeException class

---
+++ src/main/java/ru/plidia/builder/exception/NullSameIdAndAgeException.java
@@ -0,0 +1,7 @@
+package ru.plidia.builder.exception;
+
+public class NullSameIdAndAgeException extends RuntimeException {
+    public NullSameIdAndAgeException(String message) {
+        super(message);
+    }
+}
